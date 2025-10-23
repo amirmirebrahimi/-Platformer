8385bf576027038c1872f860c58d603128599d57
amirmirebrahimi <amirmirebrahimy@gmail.com>
2025-10-23 11:12:55 +0330

    adding jumping animation and moving platform

--- player.gd
+++ player.gd
@@ -9,11 +9,13 @@ func _physics_process(delta: float) -> void:
 	# Add gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
+	
+	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+		velocity.y = JUMP_VELOCITY/4
 	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	# Horizontal movement
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction != 0:
@@ -29,3 +31,16 @@ func _physics_process(delta: float) -> void:
 		anim.flip_h = direction < 0   # flip when going left
 	else:
 		anim.play("idle")
+		
+	if not is_on_floor():
+		anim.play("jump")
+		
+
+		
+		
+
+			
+
+		
+		
+		
